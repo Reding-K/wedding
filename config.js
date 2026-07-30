2263554df11bcee342618b56d6001ad47c9da6a2
Reding <kimhj8074@gmail.com>
2026-07-30 14:48:42 +0900

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,8 @@ const CONFIG = {
     ],
     bride: [
       { role: "신부", name: "김혜원", bank: "국민", number: "260201-04-218815" },
-
+      { role: "아버지", name: "김진형", bank: "국민", number: "023-21-0227-250" },
+      { role: "어머니", name: "하은미", bank: "기업", number: "220-023854-03-015" }
     ]
   },
 
