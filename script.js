9fae25761728cd75b43cd7254d778ab8fad02a42
Reding <kimhj8074@gmail.com>
2026-07-30 22:31:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -362,7 +362,7 @@ function initBgmToggle() {
     const parentsHTML = `
       <div class="parent-row">${parentLine(g.father, g.mother, g.fatherDeceased, g.motherDeceased)}의 아들 <span class="child-name">${g.name}</span>
       </div>
-      <div class="parent-row">${parentLine(b.father, b.mother, b.fatherDeceased, b.motherDeceased)}의  딸&nbsp; <span class="child-name">${b.name}</span>
+      <div class="parent-row">${parentLine(b.father, b.mother, b.fatherDeceased, b.motherDeceased)}의&nbsp;&nbsp;딸&nbsp; <span class="child-name">${b.name}</span>
       </div>
     `;
 
